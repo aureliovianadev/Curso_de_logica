1a3626b1f0c74b9efb4ca280239a5c1f5116b47f
aureliovianadev <aureliovianadev@gmail.com>
2025-08-24 11:37:56 -0300

    Adicionando exercícios de funções

--- aula8.js
+++ aula8.js
@@ -23,3 +23,28 @@ function verificaParImpar (num){
 var numero = parseInt(prompt('Digite um numero para verificar se é par ou impar'))
 
 console.log(numero + verificaParImpar(numero))
+
+// Função que calcula o fatorial de um numero
+
+function fatorial (x){
+   var fator =1 
+    for (var i=1; i<=x;i++){
+      fator *=i
+    }
+     return fator
+}
+let Nume=parseInt(prompt('Digite o número que você deseja saber o fatorial: '))
+console.log(fatorial(Nume))
+
+// Analizador de nomes
+
+function analizanome(){
+   console.log('O comprimento do seu nome é: '+nome.length)
+   console.log('O seu nome em maiúsculo é: ' + nome.toUpperCase())
+   console.log('O seu nome em minúsculo é: ' + nome.toLowerCase())
+   console.log('A primeira letra do seu nome é: ' + nome.to)
+}
+
+let nome = prompt('Digite um nome:')
+
+console.log(analizanome(nome))
